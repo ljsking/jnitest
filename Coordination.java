3063c6180e737e111d8d8b5095097835f0e3042e
Jaesung Lee <ljsking@ljsking-desktop.(none)>
2008-12-06 20:27:38 +0900

    jni is worked wel; :) good job :)

--- Coordination.java
+++ Coordination.java
@@ -1,7 +1,9 @@
 public class Coordination{
-	private int x, y;
+	public int x, y;
 	public Coordination(int X, int Y){
-		int x = X;
-		int y = Y;
+		this.x = X;
+		this.y = Y;
 	}
+	public int getX(){return this.x;}
+	public int getY(){return this.y;}
 }
--- JniBridge.cpp
+++ JniBridge.cpp
@@ -1,11 +1,14 @@
-#include <stdio.h>  
+#include <iostream>
+#include <cmath>  
 #include "JniBridge.h"
+using namespace std;
 JNIEXPORT jint JNICALL Java_JniBridge_calcDist(JNIEnv *env, jclass cls, jobject obj) 
 { 
-	jfieldID xField = env->GetFieldID(cls, "x", "I");
-	jfieldID yField = env->GetFieldID(cls, "y", "I");
+	jclass coor = env->GetObjectClass(obj);
+	jfieldID xField = env->GetFieldID(coor, "x", "I");
+	jfieldID yField = env->GetFieldID(coor, "y", "I");
 	jint x = env->GetIntField(obj, xField);
 	jint y = env->GetIntField(obj, yField);
-	printf("Get X:%d, Y:%d\n", x, y);
-	return x+y;
+	cout<<"Get x:"<<x<<" y:"<<y<<endl;
+	return sqrt(x*x+y*y);
 } 
